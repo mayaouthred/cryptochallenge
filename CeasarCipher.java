42c24a44bad6903e83f08f84e5d0c09ed17c5944
ehsimsch <37914943+ehsimsch@users.noreply.github.com>
2018-05-16 17:39:27 -0700

    Add files via upload
    added file functionality

--- CeasarCipher.java
+++ CeasarCipher.java
@@ -1,20 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CeasarCipher {
 
 	static ArrayList<String> words;
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		words = new ArrayList<String>();
-		Scanner in = new Scanner(new File("dictionary"));
+		Scanner in = new Scanner(new File("dictionary.txt"));
 		while (in.hasNextLine()) words.add(in.nextLine());
 		in.close();
-		String a = "input your code here";
-		System.out.println(Arrays.toString(decode(a)));
+		Scanner into = new Scanner(new File("input.txt"));
+		String a = "";
+		while (into.hasNextLine()) a += into.nextLine();
+		into.close();
+		PrintWriter write = new PrintWriter("output.txt", "UTF-8");
+		String[] b = decode(a);
+		String ret = "";
+		if (b != null) {
+			for (int i = 0; i < b.length; i++) {
+				ret += b[i] + " ";
+			}
+		} else {
+			ret = "null";
+		}
+		write.println(ret);
+		write.close();
 	}
 	
 	public static String[] decode(String in) {
